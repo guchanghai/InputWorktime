484796bc5b65c1b2242688c5f5bf31c4f0465157
Changhai Gu <changhaigu@gmail.com>
2017-10-02 15:00:53 -0700

    CheckSSQ.js
    Check the SSQ

---
+++ CheckSSQ.js
@@ -0,0 +1,14 @@
+/* Script to check the SSQ
+ * 
+ */
+
+$('.add-to-quote' )[0].trigger('click');
+$('.add-to-quote' )[3].trigger('click');
+$( '.next-step' )[1].trigger('click');
+$( 'input[id="first-name-input"]' ).val( 'Changhai' );
+$( 'input[id="last-name-input"]' ).val( 'gu' );
+$( 'input[id="email-input"]' ).val( 'changhaigu@gmail.com' );
+$( 'input[id="phone-input"]' ).val( '4085363641' );
+$( 'input[id="company-input"]' ).val( 'Adobe' );
+$( 'input[id="postal-input"]' ).val( '95110' );
+
